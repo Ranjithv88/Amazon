93cf60d77f94ea01a6c533a956a14a83c132b4fe
ranjithv88 <ranjithkumar22445588@gmail.com>
2024-08-10 19:43:47 +0530

    upload in git hub

---
+++ src/test/java/com/SpringBoot/Amazon/AmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.Amazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
